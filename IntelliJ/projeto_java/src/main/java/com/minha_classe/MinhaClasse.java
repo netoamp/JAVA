b477fe6ddd5a6e99dd9904d653d6fccbdcd37720
netoamp <netoamp@hotmail.com>
2023-03-19 05:14:10 -0300

    estudos

---
+++ IntelliJ/projeto_java/src/main/java/com/minha_classe/MinhaClasse.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
